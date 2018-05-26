30bf9e1717878cc3ff88364a9f76308abb54c34f
Cynopolis <quinton_henthorne19@rdale.org>
2018-05-26 01:41:43 -0500

    Some Functionality
    Added Some basic test functionality to the School Closings Button, and the Calendar Button

--- HawksGo2/app/src/main/java/com/schools/robbinsdale/hawksgo/MainActivity.java
+++ HawksGo2/app/src/main/java/com/schools/robbinsdale/hawksgo/MainActivity.java
@@ -1,21 +1,49 @@
 package com.schools.robbinsdale.hawksgo;
 
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button schoology = findViewById(R.id.schoology);
-        schoology.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        configureClosedSchools();
+        configureCalendarEvents();
 
+    }
+
+    private void configureClosedSchools(){
+
+        final Button schoolClosed = findViewById(R.id.schoolClosing);
+        schoolClosed.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, schoolClosings.class));
+            }
+        });
+    }
+
+    private void configureCalendarEvents(){
+
+        final Button openCalendar = findViewById(R.id.calendar);
+        openCalendar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Calendar.class));
             }
         });
     }
---
+++ HawksGo2/app/src/main/java/com/schools/robbinsdale/hawksgo/schoolClosings.java
@@ -0,0 +1,28 @@
+package com.schools.robbinsdale.hawksgo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class schoolClosings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_school_closings);
+
+        backButton();
+    }
+
+    private void backButton(){
+
+        final Button schoolClosed = findViewById(R.id.backButton);
+        schoolClosed.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(schoolClosings.this, MainActivity.class));
+            }
+        });
+    }
+}
